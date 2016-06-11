041895785997e22b4a4cd130037d07fa4f1be36b
Gary Readhead <gary@roomworks.co.za>
2016-06-11 21:30:19 +0200

    New Version
    New version of Phonegap

---
+++ platforms/browser/www/js/age.js
@@ -0,0 +1,103 @@
+function sendage() {
+		
+	var f = document.getElementById("u19").options;
+	
+	if (f === "u19") {
+		window.location = "u19.html";
+		}
+	
+	}	
+	
+	
+function u19() {
+	
+var d = new Date();
+var n = d.getFullYear();
+var x = document.getElementById("1").value;
+var y = n - x
+
+if (x == null || x == "") {
+	showStickyErrorToast();
+	return;
+	}
+
+if (y >= 22 || y <= 4) {
+	
+	showStickyNoticeToast();
+	return;
+	
+	}
+	
+else if( y == 19 || y == 18) {
+	document.getElementById("message").innerHTML = "<h3 class='message'>His age is " + y +"</h3>";
+	showStickySuccessToast();
+	return;	
+		
+	} else if(y <= 17){
+		showStickyWarningToast();
+		document.getElementById("message").innerHTML = "<h3 class='message_alt'> His age is " + y + "</h3>";
+		return;	
+		}
+		
+	else if(y > 19){
+		showStickyWarningToast();
+		document.getElementById("message").innerHTML = "<h3 class='message_alt'>His age is " + y + "</h3>" ;
+		return;	
+		}		
+		
+		return;	
+		
+		}
+		
+ 
+		
+		
+function u18() {
+	
+var d = new Date();
+var n = d.getFullYear();
+var x = document.getElementById("2").value;
+var y = n - x
+
+
+if (x == null || x == "" ) {
+	showStickyErrorToast();
+	return;
+	}
+	
+if (y >= 22 || y <= 4) {
+	
+	showStickyNoticeToast();
+	return;
+	
+	}
+	
+else if( y == 18 || y == 17) {
+
+	document.getElementById("message").innerHTML = "<h3 class='message'>His age is " + y +"</h3>";
+	showStickySuccessToast();
+	return;	
+		
+	} 
+	
+	else if(y <= 16){
+		showStickyWarningToast();
+		document.getElementById("message").innerHTML = "<h3 class='message_alt'>His age is " + y + "</h3>" ;
+		return;	
+		}
+		
+	else if(y > 18){
+		showStickyWarningToast();
+		document.getElementById("message").innerHTML = "<h3 class='message_alt'>His age is " + y + "</h3>";
+		return;	
+		}		
+		
+		return;	
+		
+		}	
+			
+		
+function resetage() {
+	window.location.reload(true);
+	}		
+	
---
+++ platforms/browser/www/js/reply.js
@@ -0,0 +1,72 @@
+		function showSuccessToast() {
+			$().toastmessage('showSuccessToast',
+					"Your song has been playlisted");
+		}
+		function showStickySuccessToast() {
+				$().toastmessage('showToast', {
+				text : '<i class="fa fa-thumbs-up"> Able to Play</i>',
+				sticky : true,
+				position : 'top-center',
+				type : 'success',
+				closeText : '',
+				close : function() {
+					resetage();
+				}
+			});
+
+		}
+		function showNoticeToast() {
+			$().toastmessage('showNoticeToast',
+					"Please insert program specific year range!");
+		}
+		function showStickyNoticeToast() {
+			$().toastmessage('showToast', {
+				text : 'Please insert program specific year range!',
+				sticky : true,
+				position : 'top-center',
+				type : 'notice',
+				closeText : '',
+				close : function() {
+					resetage();
+				}
+			});
+		}
+		function showWarningToast() {
+			$().toastmessage('showWarningToast',
+					"Warning Dialog which is fading away ...");
+		}
+		function showStickyWarningToast() {
+			$().toastmessage('showToast', {
+				text : 'He is not able to play',
+				sticky : true,
+				position : 'top-center',
+				type : 'warning',
+				closeText : '',
+				close : function() {
+					resetage();
+				}
+			});
+		}
+		function showErrorToast() {
+			$().toastmessage('showErrorToast',
+					"Please insert a year of birth!");
+		}
+		function showStickyErrorToast() {
+			$().toastmessage('showToast', {
+				text : 'Please insert a year of birth!',
+				sticky : true,
+				position : 'top-center',
+				type : 'error',
+				closeText : '',
+				close : function() {
+					resetage();
+				}
+			});
+		}
+
+		function reFresh() {
+
+			setTimeout(function() {
+				window.location.reload(1)
+			}, 3000);
+		}
